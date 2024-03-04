1078d1c955f995b56ec3ab45c11440b1ef074161
Grant <gbrans@uw.edu>
2024-03-03 18:13:37 -0800

    tried to make progress in article rendering

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,17 @@
 import React from "react";
 import TEXT from "../articles/articlecontent.json";
+import { useParams } from 'react-router-dom';
 
-export function Article( {match} ) {
+export function Article() {
     /* should show title of article and content by accessing TEXT, which is an array of objects with each object representing article content.
     Check src/articles/articlecontent.json to see the format. 
     Should also implement feedback form at the end of the returned div. Feedback form might have to be implemented as separate component */
 
-    /* info on react router's match: https://v5.reactrouter.com/web/api/match */ 
-
-    let {subtopic} = match.params;
+    let {subtopic} = useParams();
     let article = TEXT.find(item => item.subtopic === subtopic);
+    if (!article) {
+        return <div>Article not found.</div>;
+    }
     return (
         <div>
             <h2>{article.title}</h2>
